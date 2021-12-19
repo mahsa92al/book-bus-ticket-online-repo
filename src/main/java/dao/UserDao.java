96e9325a10fbdb6ccba185cf9505bcc99bc38845
Mahsa <mahsa.alikhany@gmail.com>
2021-12-19 20:48:19 +0330

    added user login validation.

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,32 @@
-package dao;/**
+package dao;
+
+import model.User;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
+import org.hibernate.hql.internal.ast.tree.RestrictableStatement;
+
+import java.util.List;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class UserDao {
+ */
+public class UserDao extends BaseDao{
+
+    public boolean findUserByUsernameAndPassword(String username, String password) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Criteria criteria = session.createCriteria(User.class);
+        SimpleExpression usernameCond = Restrictions.eq("username", username);
+        SimpleExpression passwordCond = Restrictions.eq("password", password);
+        Restrictions.and(usernameCond, passwordCond);
+        List list = criteria.list();
+        transaction.commit();
+        session.close();
+        if(list != null)
+            return true;
+        return false;
+    }
 }
--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,16 @@
-package service;/**
+package service;
+
+import dao.UserDao;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class UserService {
+ */
+public class UserService {
+    UserDao userDao = new UserDao();
+    public boolean userValidation(String username, String password) {
+        if(userDao.findUserByUsernameAndPassword(username, password)){
+            return true;
+        }
+        return false;
+    }
 }
