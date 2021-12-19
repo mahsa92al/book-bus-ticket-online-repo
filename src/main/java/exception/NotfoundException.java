c1f3f8c9c6f5796c5c34ed7e1882f9092e59ac11
Mahsa <mahsa.alikhany@gmail.com>
2021-12-19 11:25:48 +0330

    added counstructor to NotFoundException

--- src/main/java/exception/NotfoundException.java
+++ src/main/java/exception/NotfoundException.java
@@ -1,4 +1,10 @@
-package exception;/**
+package exception;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class NotfoundException {
+ */
+public class NotfoundException extends RuntimeException{
+    public NotfoundException(String message) {
+        super(message);
+    }
 }
