673a05d65ddab124329c384424a28e45bd8fe7e4
Mahsa <mahsa.alikhany@gmail.com>
2021-12-22 21:02:44 +0330

    modified ticketDao name to tripDao

--- src/main/java/service/TripService.java
+++ src/main/java/service/TripService.java
@@ -9,10 +9,10 @@ import java.util.List;
 /**
  * @author Mahsa Alikhani m-58
  */
-public class TicketService {
-    TripDao ticketDao = new TripDao();
+public class TripService {
+    TripDao tripDao = new TripDao();
     public List<Ticket> getAvailableTicketsInfo(String origin, String destination, Date departureDate, int pageSize) {
-        List<Ticket> ticketsInfo = ticketDao.findAvailableTicketInfo(origin, destination, departureDate, pageSize);
-        return null;
+        List<Ticket> trips = tripDao.findAvailableTicketInfo(origin, destination, departureDate, pageSize);
+        return trips;
     }
 }
