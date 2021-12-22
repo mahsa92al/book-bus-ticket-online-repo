1df75f9717cf959f3aefe03e89150b84ce653189
Mahsa <mahsa.alikhany@gmail.com>
2021-12-22 21:08:35 +0330

    added @Data lombok to TripDto

--- src/main/java/model/TripDto.java
+++ src/main/java/model/TripDto.java
@@ -1,10 +1,12 @@
 package model;
 
+import lombok.Data;
 import model.enumeration.BusType;
 
 /**
  * @author Mahsa Alikhani m-58
  */
+@Data
 public class TripDto {
      private String corporationName;
      private BusType busType;
