587d53eff3afde28e5b4f9c6bb0773513a3deff0
Mahsa <mahsa.alikhany@gmail.com>
2021-12-23 00:22:27 +0330

    added admin login

--- src/main/java/dao/AdminDao.java
+++ src/main/java/dao/AdminDao.java
@@ -1,4 +1,31 @@
-package dao;/**
+package dao;
+
+import model.Admin;
+import model.User;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.SimpleExpression;
+
+import java.util.List;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class AdminDao {
+ */
+public class AdminDao extends BaseDao{
+    public boolean findAdminByUsernameAndPassword(String username, String password) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Criteria criteria = session.createCriteria(Admin.class);
+        SimpleExpression usernameCond = Restrictions.eq("username", username);
+        SimpleExpression passwordCond = Restrictions.eq("password", password);
+        Restrictions.and(usernameCond, passwordCond);
+        List list = criteria.list();
+        transaction.commit();
+        session.close();
+        if(list != null)
+            return true;
+        return false;
+    }
 }
--- src/main/java/service/AdminService.java
+++ src/main/java/service/AdminService.java
@@ -1,4 +1,16 @@
-package service;/**
+package service;
+
+import dao.AdminDao;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class AdminService {
+ */
+public class AdminService {
+    AdminDao adminDao = new AdminDao();
+    public boolean adminValidation(String username, String password) {
+        if(adminDao.findAdminByUsernameAndPassword(username, password)){
+            return true;
+        }
+        return false;
+    }
 }
