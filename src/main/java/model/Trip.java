76604b28aa4b6c2be45557e46f550f6bd11e3754
Mahsa <mahsa.alikhany@gmail.com>
2021-12-22 20:45:21 +0330

    added some properties to Trip

--- src/main/java/model/Trip.java
+++ src/main/java/model/Trip.java
@@ -1,7 +1,32 @@
 package model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author Mahsa Alikhani m-58
  */
+@Data
+@Entity
 public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String origin;
+    private String destination;
+    @Temporal(TemporalType.DATE)
+    private Date departureDate;
+    @Temporal(TemporalType.TIME)
+    private Date departureTime;
+    private String corporationName;
+    private double price;
+    private int remainingSeats;
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<Bus> buses = new ArrayList<>();
+    @OneToMany
+    private List<Ticket> tickets = new ArrayList<>();
 }
