b08bc913dbe4a384bc4f45b82b01559f55e7c6ca
Mahsa <mahsa.alikhany@gmail.com>
2021-12-23 23:09:14 +0330

    added show bus reports feature

--- src/main/java/dao/BusDao.java
+++ src/main/java/dao/BusDao.java
@@ -1,4 +1,37 @@
-package dao;/**
+package dao;
+
+import model.Bus;
+import model.BusDto;
+import model.enumeration.BusType;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.Transformers;
+
+import java.util.List;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class BusDao {
+ */
+public class BusDao extends BaseDao{
+    public List<BusDto> findBusReportsByType(BusType busType) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Criteria criteria = session.createCriteria(Bus.class, "b");
+        criteria.createAlias("b.trip", "bt");
+        criteria.add(Restrictions.eq("bt.busType", busType));
+        criteria.addOrder(Order.asc("bt.departureDate"));
+        criteria.setProjection(Projections.projectionList()
+                .add(Projections.property("b.busPlate").as("busPlate"))
+                .add(Projections.property("bt.departureDate").as("departureDate"))
+                .add(Projections.property("bt.remainingSeats").as("remainingSeats")));
+        criteria.setResultTransformer(Transformers.aliasToBean(BusDto.class));
+        List<BusDto> reports = criteria.list();
+        transaction.commit();
+        session.close();
+        return reports;
+    }
 }
--- src/main/java/service/BusService.java
+++ src/main/java/service/BusService.java
@@ -1,4 +1,18 @@
-package service;/**
+package service;
+
+import dao.BusDao;
+import model.BusDto;
+import model.enumeration.BusType;
+
+import java.util.List;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class BusService {
+ */
+public class BusService {
+    BusDao busDao = new BusDao();
+    public List<BusDto> getBusReportsByType(BusType busType) {
+        List<BusDto> busReportsList = busDao.findBusReportsByType(busType);
+        return busReportsList;
+    }
 }
