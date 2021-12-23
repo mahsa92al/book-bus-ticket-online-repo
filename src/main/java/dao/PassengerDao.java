4f225e4fe80cdcb196dc67042d9932d285f2ff3f
Mahsa <mahsa.alikhany@gmail.com>
2021-12-23 20:55:54 +0330

    added saveNewPassenger method

--- src/main/java/dao/PassengerDao.java
+++ src/main/java/dao/PassengerDao.java
@@ -1,4 +1,18 @@
-package dao;/**
+package dao;
+
+import model.Passenger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class PassengerDao {
+ */
+public class PassengerDao extends BaseDao{
+    public void saveNewPassenger(Passenger passenger) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(passenger);
+        transaction.commit();
+        session.close();
+    }
 }
--- src/main/java/service/PassengerService.java
+++ src/main/java/service/PassengerService.java
@@ -1,4 +1,14 @@
-package service;/**
+package service;
+
+import dao.PassengerDao;
+import model.Passenger;
+
+/**
  * @author Mahsa Alikhani m-58
- */public class PassengerService {
+ */
+public class PassengerService {
+    PassengerDao passengerDao = new PassengerDao();
+    public void saveNewPassenger(Passenger passenger) {
+        passengerDao.saveNewPassenger(passenger);
+    }
 }
