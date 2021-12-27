de30e886cdcd2c1fefae62025a60e03e4996ddee
Mahsa <mahsa.alikhany@gmail.com>
2021-12-27 16:04:12 +0330

    modified getCriteria method

--- src/main/java/dao/TripDao.java
+++ src/main/java/dao/TripDao.java
@@ -8,6 +8,7 @@ import org.hibernate.Criteria;
 import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 import org.hibernate.transform.Transformers;
@@ -20,16 +21,15 @@ import java.util.List;
  * @author Mahsa Alikhani m-58
  */
 public class TripDao extends BaseDao {
-    private static final int pageIndex = 0;
 
     public List<TripDto> findAvailableTripsInfo(String origin, String destination, Date departureDate,
                                                 String corporationName, BusType busType, Double[] priceRange,
-                                                Date[] departureTimeRange, int pageSize) {
+                                                Date[] departureTimeRange, int pageSize, int startIndex) {
         Session session = sessionFactory.openSession();
         Transaction transaction = session.beginTransaction();
         Criteria criteria = getCriteria(origin, destination, departureDate, corporationName, busType,
                 priceRange, departureTimeRange, session);
-        criteria.setFirstResult(pageIndex);
+        criteria.setFirstResult(startIndex);
         criteria.setMaxResults(pageSize);
         List<TripDto> trips = criteria.list();
         transaction.commit();
@@ -40,132 +40,30 @@ public class TripDao extends BaseDao {
     private Criteria getCriteria(String origin, String destination, Date departureDate,
                                  String corporationName, BusType busType, Double[] priceRange,
                                  Date[] departureTimeRange, Session session) {
-        Criteria criteria = session.createCriteria(Trip.class, "t");
-        if (departureDate == null && corporationName == null && busType == null
-                && priceRange.length == 0 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination)));
-        }
-        if (departureDate != null && corporationName == null && busType == null
-                && priceRange.length == 0 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.departureDate", departureDate)));
-        }
-        if (departureDate == null && corporationName != null && busType == null
-                && priceRange.length == 0 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.corporationName", corporationName)));
-        }
-        if (departureDate == null && corporationName == null && busType != null
-                && priceRange.length == 0 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.busType", busType)));
-        }
-        if (departureDate == null && corporationName == null && busType == null
-                && priceRange.length == 2 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.between("t.price", priceRange[0], priceRange[1])));
-        }
-        if (departureDate == null && corporationName == null && busType == null
-                && priceRange.length == 0 && departureTimeRange.length == 2) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.between("t.departureTime", departureTimeRange[0], departureTimeRange[1])));
-        }
-        if (departureDate != null && corporationName != null && busType == null
-                && priceRange.length == 0 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.departureDate", departureDate),
-                    Restrictions.eq("t.corporationName", corporationName)));
-        }
-        if (departureDate != null && corporationName == null && busType != null
-                && priceRange.length == 0 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.departureDate", departureDate),
-                    Restrictions.eq("t.busType", busType)));
-        }
-        if (departureDate != null && corporationName == null && busType == null
-                && priceRange.length == 2 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.departureDate", departureDate),
-                    Restrictions.between("t.price", priceRange[0], priceRange[1])));
-        }
-        if (departureDate != null && corporationName == null && busType == null
-                && priceRange.length == 0 && departureTimeRange.length == 2) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.departureDate", departureDate),
-                    Restrictions.between("t.departureTime", departureTimeRange[0], departureTimeRange[1])));
-        }
-        if (departureDate == null && corporationName != null && busType != null
-                && priceRange.length == 0 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.corporationName", corporationName),
-                    Restrictions.eq("t.busType", busType)));
-        }
-        if (departureDate == null && corporationName != null && busType == null
-                && priceRange.length == 2 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.corporationName", corporationName),
-                    Restrictions.between("t.price", priceRange[0], priceRange[1])));
-        }
-        if (departureDate == null && corporationName != null && busType == null
-                && priceRange.length == 0 && departureTimeRange.length == 2) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.corporationName", corporationName),
-                    Restrictions.between("t.departureTime", departureTimeRange[0], departureTimeRange[1])));
-        }
-        if (departureDate == null && corporationName == null && busType != null
-                && priceRange.length == 2 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.busType", busType),
-                    Restrictions.between("t.price", priceRange[0], priceRange[1])));
-        }
-        if (departureDate == null && corporationName == null && busType != null
-                && priceRange.length == 0 && departureTimeRange.length == 2) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.busType", busType),
-                    Restrictions.between("t.departureTime", departureTimeRange[0], departureTimeRange[1])));
-        }
-        if (departureDate == null && corporationName == null && busType == null
-                && priceRange.length == 2 && departureTimeRange.length == 2) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.between("t.price", priceRange[0], priceRange[1]),
-                    Restrictions.between("t.departureTime", departureTimeRange[0], departureTimeRange[1])));
-        }
-        if (departureDate != null && corporationName != null && busType != null
-                && priceRange.length == 0 && departureTimeRange.length == 0) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.departureDate", departureDate),
-                    Restrictions.eq("t.corporationName", corporationName),
-                    Restrictions.eq("t.busType", busType)));
-        }
-        //for 3 and 4 are remaining:/
 
-        if (departureDate != null && corporationName != null && busType != null
-                && priceRange.length == 2 && departureTimeRange.length == 2) {
-            criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
-                    Restrictions.eq("t.destination", destination),
-                    Restrictions.eq("t.departureDate", departureDate),
-                    Restrictions.eq("t.corporationName", corporationName),
-                    Restrictions.eq("t.busType", busType),
-                    Restrictions.between("t.price", priceRange[0], priceRange[1]),
-                    Restrictions.between("t.departureTime", departureTimeRange[0], departureTimeRange[1])));
+        Criteria criteria = session.createCriteria(Trip.class, "t");
+
+        criteria.add(Restrictions.and(Restrictions.eq("t.origin", origin),
+                Restrictions.eq("t.destination", destination)));
+        if (departureDate != null) {
+            criteria.add(Restrictions.eq("t.departureDate", departureDate));
         }
+
+        if (busType != null) {
+            criteria.add(Restrictions.eq("t.busType", busType));
+        }
+
+        if (corporationName != null) {
+            criteria.add(Restrictions.eq("t.corporationName", corporationName));
+        }
+        if (priceRange.length == 0) {
+            criteria.add(Restrictions.between("t.price", priceRange[0], priceRange[1]));
+        }
+        if (departureTimeRange.length == 0) {
+            criteria.add(Restrictions.and(Restrictions.between("t.departureTime", departureTimeRange[0], departureTimeRange[1])));
+        }
+
+        criteria.addOrder(Order.asc("t.id"));
         criteria.setProjection(Projections.projectionList()
                 .add(Projections.property("t.id").as("code"))
                 .add(Projections.property("t.corporationName").as("corporationName"))
@@ -194,4 +92,17 @@ public class TripDao extends BaseDao {
         transaction.commit();
         session.close();
     }
+
+    public long findCountOfTrips(String origin, String destination, Date departureDate, String corporationName, BusType busType, Double[] priceRange, Date[] departureTimeRange) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Criteria criteriaCount = getCriteria(origin, destination, departureDate, corporationName, busType,
+                priceRange, departureTimeRange, session);
+        criteriaCount.setProjection(Projections.rowCount());
+        Long count = (Long) criteriaCount.uniqueResult();
+        transaction.commit();
+        session.close();
+        return count;
+    }
+
 }
